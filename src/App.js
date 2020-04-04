0434528be0e44184aecc2ff6850ac2c80e308371
Sejin Hwang <sejinhwang91@gmail.com>
2020-04-04 03:25:16 -0700

    added a components folder and a very basic sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Components/sidebar'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Sidebar />
         <header className="App-header">
           Get Meals
         </header>
---
+++ src/Components/sidebar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+
+class Sidebar extends Component {
+    render() {
+        return(
+            <>
+                <div className="sidebar-container">
+                    <form className="sidebar-form">
+                        <div className="sidebar-search">
+                            <label htmlFor="sidebar-search">
+                                Search by address:
+                            </label>
+                            <input className="sidebar-input" type="text" placeholder="seattle" id="address" required />
+                        </div>
+
+                        <div classname="results-within">
+                            <label htmlFor="results-within">
+                                Show Results Within (Miles)
+                            </label>
+                            <input className="search-radius" type="text" placeholder="10" id="radius" />
+                        </div>
+                    </form>
+
+
+                </div>
+            </>
+        )
+    }
+}
+
+export default Sidebar
