3a37edec5a76f0c5afbc3e178ac2d40de6c21694
Sejin Hwang <sejinhwang91@gmail.com>
2020-04-04 12:58:10 -0700

    added foundation for handleSubmit function/basic css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/header/header';
-import Sidebar from './Components/Sidebar/sidebar';
+import Sidebar from './Components/Sidebar/Sidebar';
 import Footer from './Components/footer/footer';
 
 class App extends React.Component {
