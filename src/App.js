cfe713bf414dc005b1565e0824e9678ac8b32e41
Victoria Moore <victoriamoore563@gmail.com>
2020-04-04 13:44:24 -0700

    correct spelling of sidebar to make page display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/header/header';
-import Sidebar from './Components/Sidebar/sidebar';
+import Sidebar from './Components/Sidebar/Sidebar';
 import Footer from './Components/footer/footer';
+import AddPlace from './Components/AddPlace/AddPlace'
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <Header />
         <Sidebar />
         <Footer />
+        <AddPlace />
       </div>
     );
   }
