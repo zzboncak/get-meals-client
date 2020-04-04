d9699ef1df26789b4b8c49ce94966f3517b4a191
Peggy Sturman <glamazon5@gmail.com>
2020-04-04 13:26:52 -0700

    Added more text

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,13 +4,13 @@ const About = () => {
     return (
        <div>
           <h1>About Get Meals</h1>
-          <p>Get Meals was created by a group of students at Thinkful who all of the like mind of wanting to create something that will help people. The app was born in the midst of the COVID-19 out of a want to help people easily find places where they can get free meals, the location of food banks and places that provide discounts to our military, healthcare workers, etc. </p>
+          <p>Get Meals was created by a group of students at Thinkful who all of the like mind of wanting to create something that will help people. The app was born in the midst of the COVID-19 out of a want to help people easily find places where they can get free meals, the location of food banks and places that provide discounts to our military, healthcare workers, etc.</p>
 
           <h1>Meet the Team</h1>
 
           <h2>John</h2>
           <p>Enter pix here</p>
-          <p>Enter about text here</p>
+          <p>John is a bioengineer transitioning to the software development world. He's just a nerd. When he has free time, John can be found doing something active outdoors (e.g. hiking a 14er, snowboarding). He is currently based out of Denver, CO.</p>
 
           <h2>Victoria</h2>
           <p>Enter pix here</p>
