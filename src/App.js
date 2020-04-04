7012324830fa1bb749e30c4a114b33887e80bf0a
Zachary Zboncak <zachary.zboncak@gmail.com>
2020-04-04 16:00:37 -0500

    organized basic react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/header/header';
-import Sidebar from './Components/Sidebar/sidebar';
+import HomePage from './Components/HomePage/HomePage';
 import Footer from './Components/footer/footer';
+import { Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Sidebar />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/about' component={/**fill in later */} />
+        <Route exact path='/add-location' component={/**fill in later */} />
         <Footer />
       </div>
     );
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -7,14 +7,10 @@ class HomePage extends React.Component {
     render() {
         return (
             <div>I am the home page
-                {/**Placeholder for header component */}
-
                 <section className="main-view">
                     <Sidebar />
                     <div className="map">Placeholder for Map</div>
                 </section>
-
-                {/**Placeholder for footer component */}
             </div>
         )
     }
