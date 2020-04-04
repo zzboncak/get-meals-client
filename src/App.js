ea9944ad7086e66a259b51e0b88612e73503ad86
Peggy Sturman <glamazon5@gmail.com>
2020-04-04 13:18:19 -0700

    addd about page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/header/header';
 import Sidebar from './Components/Sidebar/sidebar';
 import Footer from './Components/footer/footer';
+import About from './Components/About/About'
 
 class App extends React.Component {
   render() {
