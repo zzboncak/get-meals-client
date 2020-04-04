ebb979d564b262d3f2dd8add92c4b6ff710e7bfc
JY <johnathanlee72@hotmail.com>
2020-04-04 12:56:48 -0600

    basic styling and structure for header and footer

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './header.css';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     render() {
         return(
             <>
                 <div className="header-container">
+                    <section>
+                        {/* Placeholder for logo */}
+                        GetMealz                   
+                    </section>
+                    <section>
+                        {/* <Link to='/'>Home Page</Link>*/}
+                        Home Page
+                    </section>
+                    <section> 
+                        {/* <Link to='/AboutUs'>About Us</Link> */}
+                        About Us
+                    </section>
+                    <section> 
+                        {/* <Link to='/AddLocation'>Add Location</Link> */}
+                        Add Location
+                    </section>
                 </div>
             </>
         )
