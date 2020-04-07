2af1cd8e856df65fd63b60cc113a5cf59c0fb79f
JY <johnathanlee72@hotmail.com>
2020-04-06 21:30:55 -0600

    refactored code to allow for first deploy on Zeit

--- src/Components/footer/footer.test.js
+++ src/Components/footer/footer.test.js
@@ -4,6 +4,7 @@ import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Footer from './Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 describe(`Footer component`, () => {
     const props = {
@@ -12,13 +13,19 @@ describe(`Footer component`, () => {
 
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<Footer />, div);
+        ReactDOM.render(
+            <BrowserRouter>
+                <Footer />
+            </BrowserRouter>, div);
         ReactDOM.unmountComponentAtNode(div);
     });
 
     it('renders the Header component as expected', () => {
         const tree = renderer
-          .create(<Footer />)
+          .create(
+            <BrowserRouter>
+                <Footer />
+            </BrowserRouter>)
           .toJSON();
         expect(tree).toMatchSnapshot();  
     });
--- src/Components/header/header.test.js
+++ src/Components/header/header.test.js
@@ -4,6 +4,7 @@ import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Header from './header';
+import { BrowserRouter } from 'react-router-dom';
 
 describe(`Header component`, () => {
     const props = {
@@ -12,13 +13,19 @@ describe(`Header component`, () => {
 
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<Header />, div);
+        ReactDOM.render(
+            <BrowserRouter>
+                <Header />
+            </BrowserRouter>, div);
         ReactDOM.unmountComponentAtNode(div);
     });
 
     it('renders the Header component as expected', () => {
         const tree = renderer
-          .create(<Header />)
+          .create(
+            <BrowserRouter>
+                <Header />
+            </BrowserRouter>)
           .toJSON();
         expect(tree).toMatchSnapshot();  
     });
