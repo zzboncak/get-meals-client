f51cc3049ea256a45a4a0f72c7f30f6e7b3d8426
Victoria Moore <victoriamoore563@gmail.com>
2020-04-11 14:13:17 -0700

    imported fonts into project

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import './Fonts/Kreon-Regular.ttf'
-import '.Fonts/Ubuntu-Regular.ttf'
+import './Fonts/Ubuntu-Regular.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
