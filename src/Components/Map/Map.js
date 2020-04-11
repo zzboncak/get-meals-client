9d943f490274c684e92366fcc28316f0ba886a3f
Sejin Hwang <sejinhwang91@gmail.com>
2020-04-11 14:49:21 -0700

    replaced openlayers with google maps api(with example marker)

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,44 +1,54 @@
-import React, { Component } from 'react'
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import { defaults as DefaultControls, ZoomSlider, MousePosition, ScaleLine, OverviewMap } from 'ol/control';
-import './Map.css'
+import React from 'react'
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker
+} from 'react-google-maps'
+import './Map.css';
 
 
-class Maps extends Component {
 
-  
-  componentDidMount() {
-    var map = new Map({
-      target: "mapContainer",
-      view: new View({
-        center: [0, 0],
-        zoom: 1
-      }),
-      layers: [
-        new TileLayer({
-          source: new OSM()
-        })
-      ],
-      controls: DefaultControls().extend([
-        new ZoomSlider(),
-        new MousePosition(),
-        new ScaleLine(),
-        new OverviewMap()
-      ])
-    });
+function Map() {
+  return (
 
-  }
-
-  render() {
-    return (
-      <div className="mapContainer" id="mapContainer"> </div>
-    )
-  }
+    // default view for map on first load, (want to change to geolocation in future)
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 47.6062, lng: -122.3321 }} >
 
 
+      {/** Using example for testing a marker, (update to map through database in json form) */}
+      <Marker
+        key={1}
+        position={{
+          lat: 47.6062,
+          lng: -122.3321
+        }}
+      />
+
+
+    </GoogleMap>
+  )
 }
 
-export default Maps
+//required to get the google maps to display on page
+const WrappedMap = withScriptjs(withGoogleMap(Map))
+
+
+export default function Maps() {
+  return (
+    <div className="mapContainer">
+      {/** The css must be explicit for these cases! */}
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=
+        ${process.env.REACT_APP_GOOGLE_KEY}
+        `}
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "400px" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+
+      />
+    </div>
+  )
+}
