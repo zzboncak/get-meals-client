3ba8cc5e07e1bd7ee488344c11e66850c831c366
Zachary Zboncak <zacharyzboncak@Zacharys-MBP.hsd1.il.comcast.net>
2020-04-27 17:28:10 -0500

    added functionality for the sidebar to close when something is clicked

--- src/Components/Toolbar/Toolbar.js
+++ src/Components/Toolbar/Toolbar.js
@@ -7,25 +7,24 @@ import Logo from './Logo.png'
 
 const toolbar = props => (
   <header className="toolbar">
-  <nav className="toolbar_navigation">
-  <div className="toolbar_toggle-button">
-  <DrawerToggleButton click={props.drawerClickHandler} />
-  </div>
-  <img
-    src= {Logo}
-    alt="logo"
-    className="logo-image" />
-  {/* <div className="toolbar_logo"><a href="/">GET MEALS</a></div> */}
-  <div className="spacer" />
-  <div className="toolbar_navigation-items">
-  <ul>
-  
-  <li><NavLink to="/">Home</NavLink></li>
-  <li><NavLink to="/add-location">Add Location</NavLink></li>
-  <li><NavLink to="/About">About Us</NavLink></li>
-  </ul></div>
-  </nav>
-    </header>
+    <nav className="toolbar_navigation">
+      <div className="toolbar_toggle-button">
+      <DrawerToggleButton click={props.drawerClickHandler} />
+      </div>
+      <img
+        src={Logo}
+        alt="logo"
+        className="logo-image" />
+      <div className="spacer" />
+      <div className="toolbar_navigation-items">
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/add-location">Add Location</NavLink></li>
+          <li><NavLink to="/About">About Us</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 );
 
 export default toolbar;
